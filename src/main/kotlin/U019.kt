ef91010553e74c7ce8580f72580816a44848176b
Carl-MONNERA <cmonnera@regate.io>
2025-02-17 15:30:57 +0100

    U026

--- src/main/kotlin/U019.kt
+++ src/main/kotlin/U019.kt
@@ -1,23 +1,21 @@
 package org.example
 
-import kotlin.math.min
-
-fun sqInRect(lng: Int, wdth: Int): List<Int>? {
-    if (lng == wdth) return null
-
-    val result = mutableListOf<Int>()
-
-    var currentLength = lng
-    var currentWidth = wdth
-
-    while (currentLength > 0 && currentWidth > 0) {
-        val minSide = min(currentLength, currentWidth)
-
-        if (currentLength > currentWidth) currentLength -= minSide
-        else currentWidth -= minSide
-
-        result += minSide
-    }
-
-    return result
-}
+//fun sqInRect(lng: Int, wdth: Int): List<Int>? {
+//    if (lng == wdth) return null
+//
+//    val result = mutableListOf<Int>()
+//
+//    var currentLength = lng
+//    var currentWidth = wdth
+//
+//    while (currentLength > 0 && currentWidth > 0) {
+//        val minSide = min(currentLength, currentWidth)
+//
+//        if (currentLength > currentWidth) currentLength -= minSide
+//        else currentWidth -= minSide
+//
+//        result += minSide
+//    }
+//
+//    return result
+//}
---
+++ src/main/kotlin/U026.kt
@@ -0,0 +1,24 @@
+package org.example
+
+//fun sqInRect(x: Int, y: Int): List<Int>? {
+//    if (x == y) return null
+//    var currentXMax = x
+//    var currentYMax = y
+//    val res = mutableListOf<Int>()
+//
+//    while (currentXMax > 0 && currentYMax > 0) {
+//        if (currentXMax > currentYMax) {
+//            res.add(currentYMax)
+//            currentXMax -= currentYMax
+//        } else {
+//            res.add(currentXMax)
+//            currentYMax -= currentXMax
+//        }
+//    }
+//
+//    return res
+//}
+
+fun main() {
+//    println(sqInRect(5, 3))
+}
