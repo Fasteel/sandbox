e9124aa9ab3ab51be24a5ed19a61385f5b7c220f
carl-monnera_qonto <carl.monnera@qonto.com>
2025-02-21 16:16:13 +0100

    U030

---
+++ src/main/kotlin/U030.kt
@@ -0,0 +1,18 @@
+package org.example
+
+//fun sqInRect(lng: Int, wdth: Int): List<Int>? {
+//    if (lng == wdth) return null
+//    val result = mutableListOf<Int>()
+//
+//    var currentLength = lng
+//    var currentWidth = wdth
+//
+//    while (currentLength > 0 && currentWidth > 0) {
+//        val minSide = min(currentLength, currentWidth)
+//        if (currentLength > currentWidth) currentLength -= minSide
+//        else currentWidth -= minSide
+//        result += minSide
+//    }
+//
+//    return result
+//}
