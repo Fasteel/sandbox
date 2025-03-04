e86cda9356b7911e6f77bf786d07fa249133e968
carl-monnera_qonto <carl.monnera@qonto.com>
2025-03-04 11:42:05 +0100

    U036

---
+++ src/main/kotlin/U036.kt
@@ -0,0 +1,10 @@
+package org.example
+
+fun main() {
+}
+
+//fun numberOfPairs(gloves: List<String>) =
+//    gloves
+//        .groupBy { it }
+//        .values
+//        .sumOf { it.size / 2 }
---
+++ src/test/kotlin/U036KtTest.kt
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Test
+
+class U036KtTest {
+    @Test
+    fun sampleTests() {
+//        assertEquals(1, numberOfPairs(arrayOf("red", "red").toList()))
+//        assertEquals(0, numberOfPairs(arrayOf("red", "green", "blue").toList()))
+//        assertEquals(
+//            3,
+//            numberOfPairs(arrayOf("gray", "black", "purple", "purple", "gray", "black").toList())
+//        )
+//        assertEquals(0, numberOfPairs(emptyList<String>()))
+//        assertEquals(
+//            4,
+//            numberOfPairs(
+//                arrayOf(
+//                    "red",
+//                    "green",
+//                    "blue",
+//                    "blue",
+//                    "red",
+//                    "green",
+//                    "red",
+//                    "red",
+//                    "red"
+//                ).toList()
+//            )
+//        )
+    }
+}
