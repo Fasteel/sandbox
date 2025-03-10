ee42ded51b986a8f5ba3220f03e1a7c739583202
carl-monnera_qonto <carl.monnera@qonto.com>
2025-03-10 10:40:50 +0100

    U037

---
+++ src/main/kotlin/U037.kt
@@ -0,0 +1,20 @@
+package org.example
+
+// https://www.codewars.com/kata/5ce399e0047a45001c853c2b/train/kotlin
+
+// ls = [0, 1, 3, 6, 10]
+// res = [0, 0, 0, 0, 0, 0]
+// i = 4
+
+//fun sumParts(ls: IntArray): IntArray {
+//    if (ls.isEmpty()) return intArrayOf(0)
+//    val res = IntArray(ls.size + 1) { 0 }
+//    for (i in ls.indices.reversed()) {
+//        res[i] = res[i + 1] + ls[i]
+//    }
+//    return res
+//}
+
+fun main() {
+//    sumParts(intArrayOf(0, 1, 3, 6, 10))
+}
--- src/main/kotlin/draft.kt
+++ src/main/kotlin/draft.kt
@@ -1,4 +1,9 @@
 package org.example
 
+class Engine(val horsepower: Int)
+class Car(val engine: Engine)
+
 fun main() {
+    val e = Engine(110)
+    Car(e)
 }
